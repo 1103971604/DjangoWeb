f51ffe9dee442255bb644b06ad0497ae59a60236
赵宇杰 <1103971604@qq.com>
2019-05-30 15:33:42 +0800

    完工

--- dmeo1/dmeo1/settings.py
+++ dmeo1/dmeo1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u@_5txwcs!(2j(&8t$ug-jf)e2m#ipq7v_9+!l&^+@lgp#x547'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
